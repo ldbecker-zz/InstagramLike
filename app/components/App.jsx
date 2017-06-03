c5a5e46660ac84db645c16738d6f84b611ed9124
Lucas Becker <lucas.d.becker@gmail.com>
2017-06-03 14:01:21 -0700

    Add react/webpack and node/express framework.

---
+++ app/components/App.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+          Hello!
+        </div>
+      );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
